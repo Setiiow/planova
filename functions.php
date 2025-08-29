6ad87ccc363986f379ab8cfebc84b688a9174983
Setayesh <Setiiow87@gmail.com>
2025-08-29 23:30:27 +0330

    Some essential functions added

--- functions.php
+++ functions.php
@@ -1,33 +1,33 @@
 <?php
-function mytheme_setup()
-{
-  add_theme_support('post-thumbnails');
-  add_theme_support('custom-background');
-  add_theme_support('title-tag');
-  add_theme_support('custom-logo');
-  register_nav_menus([
-    "menu-left"  => "Menu Left",
-    "menu-right" => "Menu Right",
-  ]);
+// فعال کردن امکانات پایه
+function mytheme_setup() {
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_theme_support('custom-logo');
+    add_theme_support('menus');
 }
 add_action('after_setup_theme', 'mytheme_setup');
 
-function planova_enqueue_styles()
-{
-  wp_enqueue_style(
-    'planova-style', // Handle name
-    get_stylesheet_uri(), // This gets style.css in the root of the theme
+// ثبت منوها
+register_nav_menus([
+    'header_menu' => 'Header Menu',
+]);
 
-  );
-  wp_enqueue_script(
-    'tailwind', // Handle name
-    "https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4", // This gets style.css in the root of the theme
-
-  );
+// Enqueue CSS و Tailwind
+function mytheme_enqueue_scripts() {
+    wp_enqueue_style('mytheme-style', get_stylesheet_uri());
+    wp_enqueue_script('tailwind', 'https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4', [], false, true);
 }
-add_action('wp_enqueue_scripts', 'planova_enqueue_styles');
+add_action('wp_enqueue_scripts', 'mytheme_enqueue_scripts');
 
-
-
-
-
+// تابع تبدیل اعداد انگلیسی به فارسی
+function toPersianNumerals($input) {
+    $english = ['0','1','2','3','4','5','6','7','8','9'];
+    $persian = ['۰','۱','۲','۳','۴','۵','۶','۷','۸','۹'];
+    return str_replace($english, $persian, (string) $input);
+}
+function add_custom_roles() {
+    add_role('parent', 'والد', ['read' => true]);
+    add_role('teacher', 'معلم', ['read' => true]);
+}
+add_action('init', 'add_custom_roles');
